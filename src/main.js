3d4e08a04f27d0bb3ad5e73e3448d797ddb89e85
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-01-07 16:58:59 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ const MenuList = ["Menu1", "Menu2", "Menu3"];
 var MenuElement = document.querySelector("#menu");
 if (MenuElement) {
   for (let i = 0; i < MenuList.length; i++) {
-    MenuElement.innerHTML += `<li>${MenuList[i]}</li>`;
+    MenuElement.innerHTML += `<li class="bg-red-500">${MenuList[i]}</li>`;
   }
 }
