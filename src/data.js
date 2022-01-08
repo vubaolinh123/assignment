bd9c7855c6d5077e24be9518669cada8dc9a937e
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-01-08 13:50:18 +0700

    UPDATE Data render LAB1

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = [
+	{
+		id: 1,
+		tittle: "Vinh danh 295 sinh viên FPT Polytechnic học kì Spring 2018",
+		img: "https://i.imgur.com/fesHLbp.jpg",
+		desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+	},
+	{
+		id: 2,
+		tittle: "Vinh danh 295 sinh viên FPT Polytechnic học kì Spring 2018",
+		img: "https://i.imgur.com/fesHLbp.jpg",
+		desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+	},
+	{
+		id: 3,
+		tittle: "Vinh danh 295 sinh viên FPT Polytechnic học kì Spring 2018",
+		img: "https://i.imgur.com/fesHLbp.jpg",
+		desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh “Ong vàng Poly”, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt",
+	},
+];
+
+export default data;
