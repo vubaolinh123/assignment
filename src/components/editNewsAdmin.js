5b9a5106cf85f3bdddcfdc2fa4fc8a1aacac1638
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-01-14 00:48:20 +0700

    Update nốt giao diện edit thui

--- src/components/editNewsAdmin.js
+++ src/components/editNewsAdmin.js
@@ -7,7 +7,7 @@ const EditNewsAdmin = {
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <div class="p-5 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg " >
                 <div>Name: ${result.title}</div>
                 <div><img src="${result.img}" alt="" /></div>
                 <div>Desc: ${result.desc}</div>
